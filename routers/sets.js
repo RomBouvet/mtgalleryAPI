d56fc71f7ec056549c5e7ad3e450dd4b3e2dd1d2
Romain B <bouvet.romain18@gmail.com>
2020-12-27 01:25:15 +0100

    tried api image fetching. removed cards from sets fetching. need refactor

--- routers/sets.js
+++ routers/sets.js
@@ -4,27 +4,104 @@ let setRouter = express.Router();
 
 const db = mongoose.connection;
 db.on('error', /* throw error */ console.error.bind(console, 'connection error:'));
-db.once('open', () => {});
+db.once('open', () => { });
 
 setRouter.route('/')
   .get((req, res) => {
-    db.collection('sets').find({}, {
-      projection: {
-        _id: 0,
-        block: 1,
-        code: 1,
-        name: 1,
-        releaseDate: 1,
-        totalSetSize: 1,
-        type: 1
-      }
-    }).toArray(function (err, docs) {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-      res.status(200).json(docs);
-    });
+    db.collection('sets').aggregate(
+      [
+        {
+          $project: {
+            _id: 0,
+            block: 1,
+            code: 1,
+            name: 1,
+            releaseDate: 1,
+            totalSetSize: 1,
+            type: 1
+          }
+        },
+        {
+          $sort: {
+            releaseDate: -1,
+            _id: 1
+          }
+        }
+       /* {
+          $unwind: "$cards"
+        },
+        {
+          $addFields: {
+            "cards.sRarity": {
+              $cond: {
+                if: { $eq: ["$cards.rarity", "mythic"] },
+                then: 1,
+                else: {
+                  $cond: {
+                    if: { $eq: ["$cards.rarity", "rare"] },
+                    then: 2,
+                    else: 3
+                  }
+                }
+              }
+            }
+          }
+        },
+        {
+          $sort: {
+            "cards.sRarity": 1
+          }
+        },
+        {
+          $group: {
+            _id: "$code",
+            code: { $first: "$code" },
+            name: { $first: "$name" },
+            releaseDate: { $first: "$releaseDate" },
+            cards: { $push: "$cards" }
+          }
+        },
+        {
+          $project: {
+            code: 1,
+            name: 1,
+            releaseDate: 1,
+            thumbnailCardId: { 
+              $first: { 
+                  $ifNull: [ "$cards.identifiers.multiverseId", "unknown" ]
+              }
+            }
+          }
+        }
+        {
+          $match: {
+            "cards.rarity": "mythic"
+          }
+        },
+        {
+          $group: {
+            _id: "$code",
+            code: { $first: "$code" },
+            name: { $first: "$name" },
+            releaseDate: { $first: "$releaseDate" },
+            thumbnailCardId: { $first: "$cards.identifiers.multiverseId" },
+            test: { $first: "$sRarity" },
+            test2: { $first: "$cards.rarity" }
+          }
+        },
+        {
+          $project: {
+            _id: 0
+          }
+        }*/
+      ], { "allowDiskUse": true }
+  ).toArray(function (err, docs) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    res.status(200).json(docs);
+  });
   })
   .post((req, res) => {
     res.send('not supported');
@@ -48,7 +125,8 @@ setRouter.route('/:code')
         name: 1,
         releaseDate: 1,
         totalSetSize: 1,
-        type: 1
+        type: 1,
+        cards: 1
       }
     }).toArray(function (err, docs) {
       if (err) {
